ae4271e466d5e202e018fe933b7791d52a38dd0b
valeria <valeria.penaloza5@gmail.com>
2018-05-26 21:23:46 -0400

    modificacion css y agregando logica del dom

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
 /* Acá va tu código */
+
+function linkCod(){
+	document.getElementById("content-msg-encode").classList.remove("display_none");
+	document.getElementById("content-msg-encode").classList.add("display_block");
+	document.getElementById("content-msg-decode").classList.remove("display_block");
+	document.getElementById("content-msg-decode").classList.add("display_none");
+	document.getElementById("link-encode").classList.add("link_active");
+	document.getElementById("link-decode").classList.remove("link_active");
+}
+
+function linkDecode(){
+	document.getElementById("content-msg-decode").classList.remove("display_none");
+	document.getElementById("content-msg-decode").classList.add("display_block");
+	document.getElementById("content-msg-encode").classList.remove("display_block");
+	document.getElementById("content-msg-encode").classList.add("display_none");
+	document.getElementById("link-decode").classList.add("link_active");
+	document.getElementById("link-encode").classList.remove("link_active");
+}
