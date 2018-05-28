a706c19bd89b08bb1b539501aec04604a518873b
valeria <valeria.penaloza5@gmail.com>
2018-05-28 19:30:41 -0400

    modificacion css, html y js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
-/* Acá va tu código */
+window.onload = () =>{
+	/* Acá va tu código */
+	const linkActive = document.getElementById("link-encode");
+	const linkDesactive = document.getElementById("link-decode");
 
-function linkCod(){
-	document.getElementById("content-msg-encode").classList.remove("display_none");
-	document.getElementById("content-msg-encode").classList.add("display_block");
-	document.getElementById("content-msg-decode").classList.remove("display_block");
-	document.getElementById("content-msg-decode").classList.add("display_none");
-	document.getElementById("link-encode").classList.add("link_active");
-	document.getElementById("link-decode").classList.remove("link_active");
-}
+	linkActive.addEventListener("click",function(){
+		document.getElementById("link-encode").classList.add("link_active");
+		document.getElementById("link-decode").classList.remove("link_active");
+		document.getElementById("content-msg-encode").classList.remove("display_none");
+		document.getElementById("content-msg-encode").classList.add("display_block");
+		document.getElementById("content-msg-decode").classList.remove("display_block");
+		document.getElementById("content-msg-decode").classList.add("display_none");
+	});
+	linkDesactive.addEventListener("click",function(){
+		document.getElementById("link-encode").classList.remove("link_active");
+		document.getElementById("link-decode").classList.add("link_active");
+		document.getElementById("content-msg-decode").classList.remove("display_none");
+		document.getElementById("content-msg-decode").classList.add("display_block");
+		document.getElementById("content-msg-encode").classList.remove("display_block");
+		document.getElementById("content-msg-encode").classList.add("display_none");
+	});
 
-function linkDecode(){
-	document.getElementById("content-msg-decode").classList.remove("display_none");
-	document.getElementById("content-msg-decode").classList.add("display_block");
-	document.getElementById("content-msg-encode").classList.remove("display_block");
-	document.getElementById("content-msg-encode").classList.add("display_none");
-	document.getElementById("link-decode").classList.add("link_active");
-	document.getElementById("link-encode").classList.remove("link_active");
 }
