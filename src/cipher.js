2dcd59479a8f13673b670b71c7701ffaf7d3ca6a
valeria <valeria.penaloza5@gmail.com>
2018-05-29 18:24:07 -0400

    agregando logica cifrado

--- src/cipher.js
+++ src/cipher.js
@@ -1,6 +1,21 @@
 window.cipher = {
-	encode: () => {
+	encode: (string, offset) => {
 		/* Acá va tu código */
+		let msgCipher = "";
+		//for recorre el texto caracter por caracter
+		for(let i = 0; i < string.length; i++){
+			//reconoce espacios vacios
+			if(string.charCodeAt(i) == 32){
+				msgCipher = " ";
+			}else{
+				let textChart = (string.charCodeAt(i) - 65 + offset) %26 + 65;
+				console.log("variable textChart-->"+textChart);
+				msgCipher += String.fromCharCode(textChart).toLowerCase();
+				console.log("variable msgCipher-->"+msgCipher);
+			}
+		}
+		document.getElementById("msg-encode-check").innerHTML = msgCipher;
+		
 	},
 	decode: () => {
 		/* Acá va tu código */
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-window.onload = () =>{
-	/* Acá va tu código */
+window.onload = () => {
+
+	/*activado y desactivado de los enlaces de cada pestaña*/
+
 	const linkActive = document.getElementById("link-encode");
 	const linkDesactive = document.getElementById("link-decode");
 
@@ -11,6 +13,7 @@ window.onload = () =>{
 		document.getElementById("content-msg-decode").classList.remove("display_block");
 		document.getElementById("content-msg-decode").classList.add("display_none");
 	});
+	
 	linkDesactive.addEventListener("click",function(){
 		document.getElementById("link-encode").classList.remove("link_active");
 		document.getElementById("link-decode").classList.add("link_active");
@@ -19,5 +22,26 @@ window.onload = () =>{
 		document.getElementById("content-msg-encode").classList.remove("display_block");
 		document.getElementById("content-msg-encode").classList.add("display_none");
 	});
+
+	/*rescatando el valor del texto a codificar */
+
+	const btnTextEncode = document.getElementById("btn_encode");
+	btnTextEncode.addEventListener("click",function(){
+		
+		let textEncode = document.getElementById("box-msg-encode").value;
+		let number_offset = document.getElementById("n_offset_e").value;
+		//document.getElementById("msg-encode-check").innerHTML = textEncode;
+		window.cipher.encode(textEncode,number_offset);
+	});
+
+	/*rescatando el valor del texto a decodificar*/
+
+	const btnTextDecode = document.getElementById("btn_decode");
+	btnTextDecode.addEventListener("click",function(){
+		let textDecode = document.getElementById("box-msg-decode").value;
+		let number_offset = document.getElementById("n_offset_d").value;
+		//document.getElementById("msg-decode-check").innerHTML = textDecode;
+	});
+	
 
 }
