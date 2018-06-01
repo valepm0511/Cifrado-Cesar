36b5381f237549ae7793fe3c6e807281107f5334
valeria <valeria.penaloza5@gmail.com>
2018-06-01 09:46:33 -0400

    agrague una breve explicacion, ordene el codigo y agregue solo cifrado en mayuscula y minuscula

--- src/cipher.js
+++ src/cipher.js
@@ -1,16 +1,19 @@
 window.cipher = {
 	encode: (string,offset) => {
-		//mensaje recibido
+		//se guarda el mensaje ya cifrado
 		let msgCipher = "";
 		//inicio recorrido de caracteres
 		for(let i = 0; i < string.length; i++){
 			//identificacion de espacios vacios
 			if(string.charCodeAt(i) === 32){
 				msgCipher += " ";
-			}else{
-				//aplicando formula de cifrado
+			}else if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
+				//aplicando formula de cifrado mayúsculas
 				let textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-				//conviertiendo el texto cifrado 
+				msgCipher+=String.fromCharCode(textChar);
+			}else{
+				//aplicando formula de cifrado minúsculas
+				textChar = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
 				msgCipher+=String.fromCharCode(textChar);
 			}
 		}
@@ -19,16 +22,20 @@ window.cipher = {
 	},
 	decode: (string,offset) => {
 		//mensaje recibido
+		//se guarda el mensaje ya descifrado
 		let msgDecipher = "";
 		//inicio recorrido de caracteres
 		for(let i = 0; i < string.length; i++){
 			//identificacion de espacios vacios
 			if(string.charCodeAt(i) === 32){
 				msgDecipher += " ";
-			}else{
-				//aplicando formula de decifrado
+			}else if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
+				//aplicando formula de descifrado mayúsculas
 				let textChar = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
-				//conviertiendo el texto decifrado 
+				msgDecipher += String.fromCharCode(textChar);
+			}else{
+				//aplicando formula de descifrado minúsculas pero no funciona :'(
+				textChar = (string.charCodeAt(i) + 97 - parseInt(offset)) % 26 + 97;
 				msgDecipher += String.fromCharCode(textChar);
 			}
 		}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 window.onload = () => {
-
 	/*activado y desactivado de los enlaces de cada pestaña*/
-
 	const linkActive = document.getElementById("link-encode");
 	const linkDesactive = document.getElementById("link-decode");
-
 	linkActive.addEventListener("click",function(){
 		document.getElementById("link-encode").classList.add("link_active");
 		document.getElementById("link-decode").classList.remove("link_active");
@@ -13,7 +10,6 @@ window.onload = () => {
 		document.getElementById("content-msg-decode").classList.remove("display_block");
 		document.getElementById("content-msg-decode").classList.add("display_none");
 	});
-	
 	linkDesactive.addEventListener("click",function(){
 		document.getElementById("link-encode").classList.remove("link_active");
 		document.getElementById("link-decode").classList.add("link_active");
@@ -22,23 +18,19 @@ window.onload = () => {
 		document.getElementById("content-msg-encode").classList.remove("display_block");
 		document.getElementById("content-msg-encode").classList.add("display_none");
 	});
-
 	/*rescatando el valor del texto a codificar */
-
 	const btnTextEncode = document.getElementById("btn_encode");
 	btnTextEncode.addEventListener("click",function(){
 		let textEncode = document.getElementById("box-msg-encode").value;
-		textEncode = textEncode.toUpperCase();
+		//textEncode = textEncode.toUpperCase();
 		let number_offset = document.getElementById("n_offset_e").value;
 		document.getElementById("msg-encode-check").innerHTML = window.cipher.encode(textEncode, number_offset);
 	});
-
 	/*rescatando el valor del texto a decodificar*/
-
 	const btnTextDecode = document.getElementById("btn_decode");
 	btnTextDecode.addEventListener("click",function(){
 		let textDecode = document.getElementById("box-msg-decode").value;
-		textDecode = textDecode.toUpperCase();
+		//textDecode = textDecode.toUpperCase();
 		let number_offset = document.getElementById("n_offset_d").value;
 		document.getElementById("msg-decode-check").innerHTML = window.cipher.decode(textDecode, number_offset);
 	});
