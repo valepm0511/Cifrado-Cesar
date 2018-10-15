c7e07d7198eb1165f9acfc2b6ad11cff4b96f8d3
valeria <valeria.penaloza5@gmail.com>
2018-10-15 17:41:25 -0300

    mejoras cifrado cesar

--- docs/index.js
+++ docs/index.js
@@ -1,45 +1,45 @@
 window.onload = () => {
 
-	/*activado y desactivado de los enlaces de cada pestaña*/
+    /*activado y desactivado de los enlaces de cada pestaña*/
 
-	const linkActive = document.getElementById("link-encode");
-	const linkDesactive = document.getElementById("link-decode");
+    const linkActive = document.getElementById("link-encode");
+    const linkDesactive = document.getElementById("link-decode");
 
-	linkActive.addEventListener("click",function(){
-		document.getElementById("link-encode").classList.add("link_active");
-		document.getElementById("link-decode").classList.remove("link_active");
-		document.getElementById("content-msg-encode").classList.remove("display_none");
-		document.getElementById("content-msg-encode").classList.add("display_block");
-		document.getElementById("content-msg-decode").classList.remove("display_block");
-		document.getElementById("content-msg-decode").classList.add("display_none");
-	});
-	
-	linkDesactive.addEventListener("click",function(){
-		document.getElementById("link-encode").classList.remove("link_active");
-		document.getElementById("link-decode").classList.add("link_active");
-		document.getElementById("content-msg-decode").classList.remove("display_none");
-		document.getElementById("content-msg-decode").classList.add("display_block");
-		document.getElementById("content-msg-encode").classList.remove("display_block");
-		document.getElementById("content-msg-encode").classList.add("display_none");
-	});
+    linkActive.addEventListener("click", function() {
+        document.getElementById("link-encode").classList.add("link_active");
+        document.getElementById("link-decode").classList.remove("link_active");
+        document.getElementById("content-msg-encode").classList.remove("display_none");
+        document.getElementById("content-msg-encode").classList.add("display_block");
+        document.getElementById("content-msg-decode").classList.remove("display_block");
+        document.getElementById("content-msg-decode").classList.add("display_none");
+    });
 
-	/*rescatando el valor del texto a codificar */
+    linkDesactive.addEventListener("click", function() {
+        document.getElementById("link-encode").classList.remove("link_active");
+        document.getElementById("link-decode").classList.add("link_active");
+        document.getElementById("content-msg-decode").classList.remove("display_none");
+        document.getElementById("content-msg-decode").classList.add("display_block");
+        document.getElementById("content-msg-encode").classList.remove("display_block");
+        document.getElementById("content-msg-encode").classList.add("display_none");
+    });
 
-	const btnTextEncode = document.getElementById("btn_encode");
-	btnTextEncode.addEventListener("click",function(){
-		let textEncode = document.getElementById("box-msg-encode").value;
-		textEncode = textEncode.toUpperCase();
-		let number_offset = document.getElementById("n_offset_e").value;
-		document.getElementById("msg-encode-check").innerHTML = window.cipher.encode(textEncode, number_offset);
-	});
+    /*rescatando el valor del texto a codificar */
 
-	/*rescatando el valor del texto a decodificar*/
+    const btnTextEncode = document.getElementById("btn_encode");
+    btnTextEncode.addEventListener("click", function() {
+        let textEncode = document.getElementById("box-msg-encode").value;
+        // textEncode = textEncode.toUpperCase();
+        let number_offset = document.getElementById("n_offset_e").value;
+        document.getElementById("msg-encode-check").innerHTML = window.cipher.encode(textEncode, number_offset);
+    });
 
-	const btnTextDecode = document.getElementById("btn_decode");
-	btnTextDecode.addEventListener("click",function(){
-		let textDecode = document.getElementById("box-msg-decode").value;
-		textDecode = textDecode.toUpperCase();
-		let number_offset = document.getElementById("n_offset_d").value;
-		document.getElementById("msg-decode-check").innerHTML = window.cipher.decode(textDecode, number_offset);
-	});
+    /*rescatando el valor del texto a decodificar*/
+
+    const btnTextDecode = document.getElementById("btn_decode");
+    btnTextDecode.addEventListener("click", function() {
+        let textDecode = document.getElementById("box-msg-decode").value;
+        //textDecode = textDecode.toUpperCase();
+        let number_offset = document.getElementById("n_offset_d").value;
+        document.getElementById("msg-decode-check").innerHTML = window.cipher.decode(textDecode, number_offset);
+    });
 }
--- src/cipher.js
+++ src/cipher.js
@@ -1,48 +1,50 @@
 window.cipher = {
-	encode: (string,offset) => {
-		//se guarda el mensaje ya cifrado
-		let msgCipher = "";
-		//inicio recorrido de caracteres
-		for(let i = 0; i < string.length; i++){
-			//identificacion de espacios vacios
-			if(string.charCodeAt(i) === 32){
-				msgCipher += " ";
-			}else if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
-				//aplicando formula de cifrado mayúsculas
-				let textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
-				msgCipher+=String.fromCharCode(textChar);
-			}else{
-				//aplicando formula de cifrado minúsculas
-				textChar = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
-				msgCipher+=String.fromCharCode(textChar);
-			}
-		}
-		//retornar respuesta
-		return msgCipher;
-	},
-	decode: (string,offset) => {
-		//mensaje recibido
-		//se guarda el mensaje ya descifrado
-		let msgDecipher = "";
-		//inicio recorrido de caracteres
-		for(let i = 0; i < string.length; i++){
-			//identificacion de espacios vacios
-			if(string.charCodeAt(i) === 32){
-				msgDecipher += " ";
-			}else if(string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90){
-				//aplicando formula de descifrado mayúsculas
-				let textChar = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
-				msgDecipher += String.fromCharCode(textChar);
-			}else{
-				//aplicando formula de descifrado minúsculas pero no funciona :'(
-				textChar = (string.charCodeAt(i) + 97 - parseInt(offset)) % 26 + 97;
-				msgDecipher += String.fromCharCode(textChar);
-			}
-		}
-		//retornar respuesta
-		return msgDecipher;
-	}
+    encode: (string, offset) => {
+        //mensaje recibido
+        let msgCipher = "";
+        for (let i = 0; i < string.length; i++) {
+            let character = string[i];
+            if (character.match(/[a-z]/i)) {
+                if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                    let textChar = (string.charCodeAt(i) - 65 + parseInt(offset)) % 26 + 65;
+                    msgCipher += String.fromCharCode(textChar);
+                } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                    textChar = (string.charCodeAt(i) - 97 + parseInt(offset)) % 26 + 97;
+                    msgCipher += String.fromCharCode(textChar);
+                }
+            } else {
+                msgCipher += character;
+            }
+        }
+        return msgCipher;
+    },
+    decode: (string, offset) => {
+        //mensaje recibido
+        let msgDecipher = "";
+
+        //inicio recorrido de caracteres
+        for (let i = 0; i < string.length; i++) {
+            let characterD = string[i];
+            //identificacion de espacios vacios
+            if (characterD.match(/[a-z]/i)) {
+                console.log(string.charCodeAt(i));
+
+                if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+                    let textChar = (string.charCodeAt(i) + 65 - parseInt(offset)) % 26 + 65;
+                    msgDecipher += String.fromCharCode(textChar);
+                    console.log("entra");
+                } else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+                    let textChar = ((string.charCodeAt(i) - 97 - parseInt(offset) + 52) % 26) + 97;
+                    msgDecipher += String.fromCharCode(textChar);
+                }
+
+
+
+            } else {
+                msgDecipher += characterD;
+            }
+        }
+        //retornar respuesta
+        return msgDecipher;
+    }
 }
-
-
-
